a55768089a534faed355ed5d7aede713de8e84b4
DISHU01032002 <dishuagarwal32@gmail.com>
2026-01-29 14:53:50 +0530

    UPDATED

--- CREATE EMPLOYEE TABLE USING DROP,  ALTER , TRUNCATE..sql
+++ CREATE EMPLOYEE TABLE USING DROP,  ALTER , TRUNCATE..sql
@@ -1,7 +1,7 @@
 DROP TABLE employee;
 
 CREATE TABLE employee(
-             employee_id SERIAL PRIMARY KEY,
+             employee_id INT PRIMARY KEY,
 			 name VARCHAR(50) NOT NULL,
 			 postiion VARCHAR(50),
 			 department VARCHAR(50),
@@ -12,12 +12,12 @@ CREATE TABLE employee(
 
 SELECT * FROM employee;
 
-INSERT INTO employee(name,position,department,hire_date,salary)
-            VALUES ('Dishu Agarwal', 'Data Analyst', 'Data Science','2020-01-07',1500.00),
-			      ('Vivek Gupta', 'Web Developer','Software Engineer','2021-05-15',18500.00),
-				  ('Om Gupta','Animator','Graphic Designer','2023-03-08',21000.00),
-				  ('Subh Agarwal','Bussiness Analyst','Data Science','2023-04-02',25000),
-				  ('Daksh Obroy','Video Editor','Graphic Designer','2025-01-09',23000);
+INSERT INTO employee(employee_id,name,position,department,hire_date,salary)
+            VALUES (101,'Dishu Agarwal', 'Data Analyst', 'Data Science','2020-01-07',15000.00),
+			      (102,'Vivek Gupta', 'Web Developer','Software Engineer','2021-05-15',18500.00),
+				  (103,'Om Gupta','Animator','Graphic Designer','2023-03-08',21000.00),
+				  (104,'Subh Agarwal','Bussiness Analyst','Data Science','2023-04-02',25000),
+				  (105,'Daksh Obroy','Video Editor','Graphic Designer','2025-01-09',23000);
 
 ALTER TABLE employee 
 RENAME COLUMN postiion TO position;
@@ -26,6 +26,11 @@ TRUNCATE TABLE employee;
 
 TRUNCATE TABLE employee RESTART IDENTITY;
 
+DELETE FROM employee
+WHERE department='Graphic Designer';
 
+
+ALTER TABLE employee
+DROP COLUMN hire_date;
 
 
